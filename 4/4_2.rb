17ce89d03d5d32dabbf44acc25f6c90d1803e905
WarHog <tiermes@yandex.ru>
2011-09-25 00:24:58 +0400

    4.2 exercise is solved

---
+++ 4/4_2.rb
@@ -0,0 +1,7 @@
+# map in this construction is redundant. It's just an artefact from previous exercise,
+# because statement says about the same method for both exercises.
+def total(numbers)
+  numbers.map { |n| n.to_i }.inject { |i, sum| i + sum }
+end
+
+puts "Sum of all numbers from 1 to 1000 inclusively equals #{total 1..1_000}"
