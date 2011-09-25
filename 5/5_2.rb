ffaf4a3834c61380dd0d7e7a3a32e30ffdad3755
WarHog <tiermes@yandex.ru>
2011-09-25 23:49:37 +0400

    Additional requirement for 5.2 task has been completed.

--- 5/5_2.rb
+++ 5/5_2.rb
@@ -1,5 +1,5 @@
 freq = {}
 words = readlines.map { |l| l.chomp }.each { |w| freq[w] = freq[w] ? freq[w] + 1 : 1 }
-freq.each do |key, value|
+freq.sort_by { |k, v| v }.reverse.each do |key, value|
   puts "#{key} word has seen #{value} times."
 end
